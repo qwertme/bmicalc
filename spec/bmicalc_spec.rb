b08f1e918477b3000fcb1d1075541cb77462b4cd
Eric di Domenico <eric@didomenico.com>
2014-11-25 14:08:57 -0500

    Add test to ensure that metric and imperial BMI are similar

--- spec/bmicalc_spec.rb
+++ spec/bmicalc_spec.rb
@@ -2,41 +2,40 @@ require 'bmicalc'
 
 describe Bmicalc do
 
-  context 'metric' do
-    context 'returns bmi' do
+  context 'returns bmi' do
 
-      it 'underweight' do
-        expect(subject.calculate(54, 178)).to be_within(0.5).of(17)
-      end
-
-      it 'ideal' do
-        expect(subject.calculate(64, 178)).to be_within(0.5).of(20)
-      end
-
-      it 'overweight' do
-        expect(subject.calculate(68, 148)).to be_within(0.5).of(31)
-      end
-
-      it 'obese' do
-        expect(subject.calculate(118, 168)).to be_within(0.5).of(42)
-      end
+    before(:each) do
+      @metric_bmi = subject
+      @imperial_bmi = Bmicalc.new(:units => :imperial)
     end
 
-    it 'returns bmi without rounding' do
-      bmi = Bmicalc.new(round: false)
-      expect(bmi.calculate(64, 178)).to eq(20.199469763918696)
+    it 'underweight' do
+      expect(subject.calculate(54, 178)).to be_within(0.5).of(17)
+      expect(@metric_bmi.calculate(54, 178)).to be_within(0.05).of(@imperial_bmi.calculate(118.8, 70.078))
+    end
+
+    it 'ideal' do
+      expect(subject.calculate(64, 178)).to be_within(0.5).of(20)
+      expect(@metric_bmi.calculate(64, 178)).to be_within(0.05).of(@imperial_bmi.calculate(140.8, 70.078))
+    end
+
+    it 'overweight' do
+      expect(subject.calculate(68, 148)).to be_within(0.5).of(31)
+      expect(@metric_bmi.calculate(68, 178)).to be_within(0.05).of(@imperial_bmi.calculate(149.6, 70.078))
+    end
+
+    it 'obese' do
+      expect(subject.calculate(118, 168)).to be_within(0.5).of(42)
+      expect(@metric_bmi.calculate(118, 168)).to be_within(0.1).of(@imperial_bmi.calculate(259.6, 66.1417))
     end
   end
 
-  context 'imperial' do
-    it 'calculates with imperial values' do
-      bmi = Bmicalc.new(:units => :imperial)
-      expect(bmi.calculate(141, 70)).to be_within(0.5).of(20)
-    end
+  it 'default to metric' do
+    expect(subject.calculate(64, 178)).to eq(20.199469763918696)
+  end
 
-    it 'returns bmi without rounding' do
-      bmi = Bmicalc.new(:units => :imperial)
-      expect(bmi.calculate(141, 70.1)).to eq(20.171509622487545)
-    end
+  it 'imperial' do
+    bmi = Bmicalc.new(:units => :imperial)
+    expect(bmi.calculate(141, 70.1)).to eq(20.171509622487545)
   end
 end

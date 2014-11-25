13cf8130a307be2be362f58f2b7fd147e3fe4145
Eric di Domenico <eric@didomenico.com>
2014-11-25 14:32:09 -0500

    Add WHO classifications

--- lib/bmicalc.rb
+++ lib/bmicalc.rb
@@ -2,6 +2,22 @@ require "bmicalc/version"
 
 class Bmicalc
 
+  CLASSIFICATIONS = {
+    18.5 => 'underweight',
+    25.0 => 'normal',
+    30.0 => 'overweight'
+  }
+
+  SUB_CLASSIFICATIONS = {
+      16.0 => 'severe thinness',
+      17.0 => 'moderate thinness',
+      18.5 => 'mild thinness',
+      25.0 => 'normal',
+      30.0 => 'pre-obese',
+      35.0 => 'obese class I',
+      40.0 => 'obese class II'
+  }
+
   class MetricBMI
     def calculate(weight, height)
       h = height.to_f / 100
@@ -15,12 +31,30 @@ class Bmicalc
     end
   end
 
-
   def initialize(options = {})
     @bmi_calculator = options.fetch(:units, :metric) == :metric ? MetricBMI.new : ImperialBMI.new
   end
 
   def calculate(weight, height)
     @bmi_calculator.calculate(weight, height)
+  end
+
+  def classification(bmi)
+    find_classification(CLASSIFICATIONS, 'obese', bmi)
+  end
+
+  def sub_classification(bmi)
+    find_classification(SUB_CLASSIFICATIONS, 'obese class III', bmi)
+  end
+
+  private
+  def find_classification(classifications, default, bmi)
+    classifications.each do |bmi_limit, name|
+      if bmi < bmi_limit
+        return name
+      end
+    end
+
+    return default
   end
 end
--- spec/bmicalc_spec.rb
+++ spec/bmicalc_spec.rb
@@ -12,21 +12,38 @@ describe Bmicalc do
     it 'underweight' do
       expect(subject.calculate(54, 178)).to be_within(0.5).of(17)
       expect(@metric_bmi.calculate(54, 178)).to be_within(0.05).of(@imperial_bmi.calculate(118.8, 70.078))
+      expect(subject.classification(18.49)).to eq('underweight')
     end
 
-    it 'ideal' do
+    it 'normal' do
       expect(subject.calculate(64, 178)).to be_within(0.5).of(20)
       expect(@metric_bmi.calculate(64, 178)).to be_within(0.05).of(@imperial_bmi.calculate(140.8, 70.078))
+      expect(subject.classification(18.50)).to eq('normal')
+      expect(subject.classification(24.99)).to eq('normal')
     end
 
     it 'overweight' do
       expect(subject.calculate(68, 148)).to be_within(0.5).of(31)
       expect(@metric_bmi.calculate(68, 178)).to be_within(0.05).of(@imperial_bmi.calculate(149.6, 70.078))
+      expect(subject.classification(25)).to eq('overweight')
+      expect(subject.classification(29.99)).to eq('overweight')
     end
 
     it 'obese' do
       expect(subject.calculate(118, 168)).to be_within(0.5).of(42)
       expect(@metric_bmi.calculate(118, 168)).to be_within(0.1).of(@imperial_bmi.calculate(259.6, 66.1417))
+      expect(subject.classification(30)).to eq('obese')
+    end
+
+    it 'should have sub classifications' do
+      expect(subject.sub_classification(15.99)).to eq('severe thinness')
+      expect(subject.sub_classification(16.99)).to eq('moderate thinness')
+      expect(subject.sub_classification(18.49)).to eq('mild thinness')
+      expect(subject.sub_classification(24.99)).to eq('normal')
+      expect(subject.sub_classification(29.99)).to eq('pre-obese')
+      expect(subject.sub_classification(34.99)).to eq('obese class I')
+      expect(subject.sub_classification(39.99)).to eq('obese class II')
+      expect(subject.sub_classification(40)).to eq('obese class III')
     end
   end
 
